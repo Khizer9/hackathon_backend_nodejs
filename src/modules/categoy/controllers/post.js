4c4dd176d4844bdac91748490caa707be8ad8d65
Khizer9 <khizer358@gmail.com>
2024-05-13 21:02:45 +0500

    nodejs boilerplate

--- src/modules/categoy/controllers/post.js
+++ src/modules/categoy/controllers/post.js
@@ -3,7 +3,6 @@ import postData from '../services/post.js';
 
 const postController = async (req, res) => {
     try {
-        console.log("req.body", req.body);
         const data = await postData(req.body);
         res.status(200).send({ status: 200, data });
     } catch (err) {
